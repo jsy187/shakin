ceb7d11c4d897ff08da424b4ac54a947b23525ac
Egor Shakin <redzone14@mail.ru>
2024-12-17 22:48:28 +0300

    функции для ввода данных о трубе и КС

--- shakin/shakin/shakin.cpp
+++ shakin/shakin/shakin.cpp
@@ -20,3 +20,28 @@ struct Station {
     double efficiency;      // Показатель эффективности
 };
 
+
+// Функция для ввода данных о трубе
+void inputPipe(Pipe& pipe) {
+    cout << "Введите название трубы: ";
+    getline(cin >> ws, pipe.name);  // Ввод с пробелами
+    cout << "Введите длину трубы: ";
+    cin >> pipe.length;
+    cout << "Введите диаметр трубы: ";
+    cin >> pipe.diameter;
+    cout << "Находится ли труба в ремонте? (1 - да, 0 - нет): ";
+    cin >> pipe.inRepair;
+}
+
+// Функция для ввода данных о КС
+void inputStation(Station& station) {
+    cout << "Введите название КС: ";
+    getline(cin >> ws, station.name);
+    cout << "Введите общее количество цехов: ";
+    cin >> station.totalShops;
+    cout << "Введите количество работающих цехов: ";
+    cin >> station.workingShops;
+    cout << "Введите показатель эффективности: ";
+    cin >> station.efficiency;
+}
+
