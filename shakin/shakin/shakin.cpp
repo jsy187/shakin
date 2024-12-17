37e794c0dbe32aac8c0b468d5c73e3ca30d51ffd
Egor Shakin <redzone14@mail.ru>
2024-12-17 22:43:23 +0300

    структуры для описания трубы и КС

---
+++ shakin/shakin/shakin.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+// Структура для описания трубы
+struct Pipe {
+    string name;      // Название (с поддержкой пробелов)
+    double length;    // Длина
+    double diameter;  // Диаметр
+    bool inRepair;    // Признак "в ремонте"
+};
+
+// Структура для описания КС
+struct Station {
+    string name;            // Название
+    int totalShops;         // Общее количество цехов
+    int workingShops;       // Количество работающих цехов
+    double efficiency;      // Показатель эффективности
+};
+
